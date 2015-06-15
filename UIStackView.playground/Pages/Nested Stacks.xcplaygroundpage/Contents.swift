61e3aa8ec6a594eb2e3fce9a97747d8ce1cc0dcd
Chris Truman <cleetruman@gmail.com>
2015-06-15 14:05:41 -0700

    Page 2 regarding Nested Stacks

---
+++ UIStackView.playground/Pages/Nested Stacks.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+//: [Previous](@previous)
+//: # Nested Stacks
+
+import UIKit
+import XCPlayground
+
+if #available(iOS 9, *) {
+    var stackViewContainer = UIStackView(frame: CGRectMake(0, 0, 500, 500))
+    stackViewContainer.axis = .Horizontal
+    stackViewContainer.distribution = .FillEqually
+
+    var stackViewColumn1 = UIStackView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500))
+    stackViewColumn1.axis = .Vertical
+    stackViewColumn1.distribution = .FillEqually
+    
+    var view1 = UIView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500/2))
+    view1.backgroundColor = .greenColor()
+    var view2 = UIView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500/2))
+    view2.backgroundColor = .yellowColor()
+    
+    stackViewColumn1.addArrangedSubview(view1)
+    stackViewColumn1.addArrangedSubview(view2)
+    
+    var stackViewColumn2 = UIStackView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500))
+    stackViewColumn2.axis = .Vertical
+    stackViewColumn2.distribution = .FillEqually
+    
+    var view3 = UIView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500/2))
+    view3.backgroundColor = .whiteColor()
+    var view4 = UIView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500/2))
+    view4.backgroundColor = .blackColor()
+    
+    stackViewColumn2.addArrangedSubview(view3)
+    stackViewColumn2.addArrangedSubview(view4)
+
+    stackViewContainer.addArrangedSubview(stackViewColumn1)
+    stackViewContainer.addArrangedSubview(stackViewColumn2)
+    
+    XCPShowView("", view: stackViewContainer)
+}
+//: [Next](@next)
