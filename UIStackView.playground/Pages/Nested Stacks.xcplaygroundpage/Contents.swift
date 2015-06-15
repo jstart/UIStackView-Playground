79ffa85645f45d547fbc42e19aa461681c0cb616
Chris Truman <cleetruman@gmail.com>
2015-06-15 14:41:08 -0700

    Sizing

--- UIStackView.playground/Pages/Nested Stacks.xcplaygroundpage/Contents.swift
+++ UIStackView.playground/Pages/Nested Stacks.xcplaygroundpage/Contents.swift
@@ -7,28 +7,31 @@ import XCPlayground
 if #available(iOS 9, *) {
     var stackViewContainer = UIStackView(frame: CGRectMake(0, 0, 500, 500))
     stackViewContainer.axis = .Horizontal
+    stackViewContainer.spacing = 10
     stackViewContainer.distribution = .FillEqually
 
-    var stackViewColumn1 = UIStackView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500))
+    var stackViewColumn1 = UIStackView(frame: CGRectMake(0, 0, (stackViewContainer.frame.size.width - 10)/2, 500))
     stackViewColumn1.axis = .Vertical
+    stackViewColumn1.spacing = 10
     stackViewColumn1.distribution = .FillEqually
     
-    var view1 = UIView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500/2))
+    var view1 = UIView(frame: CGRectMake(0, 0, (stackViewContainer.frame.size.width - 10)/2, 495/2))
     view1.backgroundColor = .greenColor()
-    var view2 = UIView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500/2))
+    var view2 = UIView(frame: CGRectMake(0, 0, (stackViewContainer.frame.size.width - 10)/2, 495/2))
     view2.backgroundColor = .yellowColor()
     
     stackViewColumn1.addArrangedSubview(view1)
     stackViewColumn1.addArrangedSubview(view2)
     
-    var stackViewColumn2 = UIStackView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500))
+    var stackViewColumn2 = UIStackView(frame: CGRectMake(0, 0, (stackViewContainer.frame.size.width - 10)/2, 500))
     stackViewColumn2.axis = .Vertical
+    stackViewColumn2.spacing = 10
     stackViewColumn2.distribution = .FillEqually
     
-    var view3 = UIView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500/2))
+    var view3 = UIView(frame: CGRectMake(0, 0, (stackViewContainer.frame.size.width - 10)/2, 495/2))
     view3.backgroundColor = .whiteColor()
-    var view4 = UIView(frame: CGRectMake(0, 0, stackViewContainer.frame.size.width/2, 500/2))
-    view4.backgroundColor = .blackColor()
+    var view4 = UIView(frame: CGRectMake(0, 0, (stackViewContainer.frame.size.width - 10)/2, 495/2))
+    view4.backgroundColor = .blueColor()
     
     stackViewColumn2.addArrangedSubview(view3)
     stackViewColumn2.addArrangedSubview(view4)
