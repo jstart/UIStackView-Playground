83a6c6d237b89482319972019ae3d0d2d64759cf
Chris Truman <cleetruman@gmail.com>
2015-06-15 19:45:22 -0700

    Markup

--- UIStackView.playground/Pages/Practical Stacks.xcplaygroundpage/Contents.swift
+++ UIStackView.playground/Pages/Practical Stacks.xcplaygroundpage/Contents.swift
@@ -3,7 +3,8 @@
 import UIKit
 import XCPlayground
 //: To use a XIB in a playground you can recompile with this command:
-//: >/Applications/Xcode-beta.app/Contents/Developer/usr/bin/ibtool --compile View.nib View.xib")
+//: >`/Applications/Xcode-beta.app/Contents/Developer/usr/bin/ibtool --compile View.nib View.xib")`
+
 //: Then you can replace the View.nib in the resources in this playground to see it rendered in the timeline
 let path = NSBundle.mainBundle().bundlePath.stringByAppendingPathComponent("Contents/Resources/View.xib")
 
