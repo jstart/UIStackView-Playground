27bb9bbea6629d9936240e5f1d260ee76471055e
Chris Truman <cleetruman@gmail.com>
2015-06-24 11:38:26 -0700

    Cleanup

--- UIStackView.playground/Pages/Alignment & Distribution.xcplaygroundpage/Contents.swift
+++ UIStackView.playground/Pages/Alignment & Distribution.xcplaygroundpage/Contents.swift
@@ -9,15 +9,12 @@ var greenView = smallViewWithColor(.greenColor())
 var yellowView = smallViewWithColor(.yellowColor())
 
 if #available(iOS 9, *) {
-    var stackView = UIStackView(frame: CGRectMake(0, 0, redView.frame.size.width, redView.frame.size.height*4))
+    var stackView = UIStackView(arrangedSubviews: [redView, blueView, greenView, yellowView])
+    stackView.frame = CGRectMake(0, 0, redView.frame.size.width, redView.frame.size.height*4)
     stackView.axis = .Vertical
     stackView.spacing = 20
     stackView.alignment = UIStackViewAlignment.Fill
     stackView.distribution = UIStackViewDistribution.FillEqually
-    stackView.addArrangedSubview(redView)
-    stackView.addArrangedSubview(blueView)
-    stackView.addArrangedSubview(greenView)
-    stackView.addArrangedSubview(yellowView)
     
     XCPShowView("Distribution", view: stackView)
 }
